e6f7d6ad24bdc225f81ed0ea03ee3079589a3278
Nguyễn Quang Sỹ <83626285+nguyenquangsy@users.noreply.github.com>
2021-12-18 08:03:59 +0700

    up

---
+++ TSP_Covid21 - Vietnam/TSP_Covid21/Areas/Test/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TSP_Covid21.Models.ShopEntity;
+
+namespace TSP_Covid21.Areas.Test.Controllers
+{
+    public class TestController : Controller
+    {
+        private COVIDEntities db;
+
+        public TestController()
+        {
+            db = new COVIDEntities();
+        }
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sensors(string user)
+        {
+            var result = from a in db.ww_TEST_Detail
+                         where a.User == user
+                         select new InfSensor()
+                         {
+                             SensorName = a.ww_TEST_Sensor.SensorPara,
+                             SensorDes = a.ww_TEST_Sensor.SensorDes,
+                         };
+
+
+            return PartialView(result);
+        }
+
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+    }
+
+    public class InfSensor
+    {
+        public string SensorName { get; set; }
+        public string SensorDes { get; set; }
+    }
+}
---
+++ TSP_Covid21 - Vietnam/TSP_Covid21/Areas/Test/TestAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace TSP_Covid21.Areas.Test
+{
+    public class TestAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Test";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Test_Sensor",
+                "Sensor",
+                new { controller = "Test", action = "Index", id = UrlParameter.Optional }
+            );
+
+            context.MapRoute(
+                "Test_default",
+                "Test/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
